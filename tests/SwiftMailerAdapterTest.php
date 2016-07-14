7ab59335f7a99fd8e2f922702979f03b08e1766d
Grasseh <steve.gagnev4si@gmail.com>
2016-07-14 15:00:08 -0400

    Added SwiftMailerAdapter

--- tests/SwiftMailerAdapterTest.php
+++ tests/SwiftMailerAdapterTest.php
@@ -13,14 +13,54 @@ namespace Tests;
 
 use Anekdotes\Mailer\Adapters\SwiftMailer\SwiftMailerAdapter;
 use PHPUnit_Framework_TestCase;
+use Swift_Mailer;
+use Swift_SmtpTransport;
 
 class SwiftMailerAdapterTest extends PHPUnit_Framework_TestCase
 {
     //Tests the instantion of the SwiftMailer Adapter
     public function testInstantiateSwiftMailerAdapter()
     {
-        $stub = $this->createMock(SwiftMailer::class);
+        $stub = $this->createMock(Swift_Mailer::class);
         $swiftMailerAdapter = new SwiftMailerAdapter($stub);
         $this->assertInstanceOf(SwiftMailerAdapter::class, $swiftMailerAdapter);
     }
+
+    //Sets the Adapter with AlwaysFrom
+    public function testAlwaysFromSwiftMailerAdapter()
+    {
+        $stub = $this->createMock(Swift_Mailer::class);
+        $swiftMailerAdapter = new SwiftMailerAdapter($stub);
+        $swiftMailerAdapter->alwaysFrom('a@b.c', 'abc');
+        $reflection = new \ReflectionClass($swiftMailerAdapter);
+        $reflectionProperty = $reflection->getProperty('from');
+        $reflectionProperty->setAccessible(true);
+        $this->assertEquals(
+          ['address' => 'a@b.c', 'name' => 'abc'],
+          $reflectionProperty->getValue($swiftMailerAdapter)
+        );
+    }
+
+    //Tests that send Sends a message
+    public function testSend()
+    {
+        $swiftMailer = new \ReflectionClass('Swift_Mailer');
+        $transportReflection = $swiftMailer->getProperty('_transport');
+        $swiftMailerStub = $this->createMock(Swift_Mailer::class);
+        $swiftMailerStub->expects($this->once())
+                   ->method('send')
+                   ->willReturn(true);
+        $transportStub = $this->getMockBuilder(Swift_SmtpTransport::class)
+                           ->setMethods(['send', '__construct'])
+                           ->disableOriginalConstructor()
+                           ->getMock();
+        $transportReflection->setAccessible(true);
+        $transportReflection->setValue($swiftMailerStub, $transportStub);
+        $swiftMailerAdapter = new SwiftMailerAdapter($swiftMailerStub);
+        $this->assertTrue(
+            $swiftMailerAdapter->send('<div>blabla</div>', function ($message) {
+                $message->to('a@b.c', 'abc');
+            }));
+    }
 }
+
