005ed9a24c907bdec4c1b8c62d37ec6a9c15316a
Mathieu Gosselin <mathieu.gosselin@anekdotes.com>
2017-09-14 16:23:37 -0400

    1.2.5 - temporary fix (problem with the helpers)

--- src/anekdotes/Adapters/Sendgrid/SendGridEmailAdapter.php
+++ src/anekdotes/Adapters/Sendgrid/SendGridEmailAdapter.php
@@ -134,7 +134,7 @@ class SendGridEmailAdapter
     {
         if (File::exists($file)) {
             $filename = explode('/', $file)[count(explode('/', $file)) - 1];
-            $mime = mime_content_type($file);
+            $mime = $this->mime_content_type($file);
             $attachment = new \Sendgrid\Attachment();
             $file_encoded = base64_encode(File::get($file));
             $attachment->setContent($file_encoded);
@@ -145,5 +145,73 @@ class SendGridEmailAdapter
         }
 
         return $this;
+    }
+
+    private function mime_content_type($filename)
+    {
+        $mime_types = [
+            'txt' => 'text/plain',
+            'htm' => 'text/html',
+            'html' => 'text/html',
+            'php' => 'text/html',
+            'css' => 'text/css',
+            'js' => 'application/javascript',
+            'json' => 'application/json',
+            'xml' => 'application/xml',
+            'swf' => 'application/x-shockwave-flash',
+            'flv' => 'video/x-flv',
+
+            // images
+            'png' => 'image/png',
+            'jpe' => 'image/jpeg',
+            'jpeg' => 'image/jpeg',
+            'jpg' => 'image/jpeg',
+            'gif' => 'image/gif',
+            'bmp' => 'image/bmp',
+            'ico' => 'image/vnd.microsoft.icon',
+            'tiff' => 'image/tiff',
+            'tif' => 'image/tiff',
+            'svg' => 'image/svg+xml',
+            'svgz' => 'image/svg+xml',
+
+            // archives
+            'zip' => 'application/zip',
+            'rar' => 'application/x-rar-compressed',
+            'exe' => 'application/x-msdownload',
+            'msi' => 'application/x-msdownload',
+            'cab' => 'application/vnd.ms-cab-compressed',
+
+            // audio/video
+            'mp3' => 'audio/mpeg',
+            'qt' => 'video/quicktime',
+            'mov' => 'video/quicktime',
+
+            // adobe
+            'pdf' => 'application/pdf',
+            'psd' => 'image/vnd.adobe.photoshop',
+            'ai' => 'application/postscript',
+            'eps' => 'application/postscript',
+            'ps' => 'application/postscript',
+
+            // ms office
+            'doc' => 'application/msword',
+            'rtf' => 'application/rtf',
+            'xls' => 'application/vnd.ms-excel',
+            'ppt' => 'application/vnd.ms-powerpoint',
+
+            // open office
+            'odt' => 'application/vnd.oasis.opendocument.text',
+            'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+        ];
+
+        $explodedfile = explode('.', $filename);
+        $ext = strtolower(array_pop($explodedfile));
+
+        if (array_key_exists($ext, $mime_types)) {
+            return $mime_types[$ext];
+        }
+        else {
+            return 'application/octet-stream';
+        }
     }
 }
